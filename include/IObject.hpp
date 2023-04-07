ad39b85e73144242d9dccc8dd6963f76ad8aa5ce
iMeaNz <mehdy.morvan@epitech.eu>
2023-04-07 19:57:58 +0200

    DOCS: Added documentation

--- include/IObject.hpp
+++ include/IObject.hpp
@@ -8,32 +8,121 @@
 #pragma once
 
 #include "IDisplayModule.hpp"
-
+/**
+ * @brief This namespace will contain all of the objects of the project
+ */
 namespace object {
+    /**
+     * @brief Enum storing all of our objects types (even if in my honest opinion,
+     * you can do basically everything using only rectangles)
+     *
+     */
     enum Type {
         RECTANGLE,
     };
-
+    /**
+     * @brief This interface is extremely important, it will hold all of the necessary
+     * informations of our objects used when drawing them. If you wish to add a new
+     * object, make sure to notify others members, so we can update our enums
+     */
     class IObject {
         public:
+            /**
+             * @brief Get the Type object
+             *
+             * @return Type
+             */
             virtual Type getType() const = 0;
-
+            /**
+             * @brief Set the Pos object
+             *
+             * @param pos
+             */
             virtual void setPos(display::Vector2i pos) = 0;
+            /**
+             * @brief Get the Pos object
+             *
+             * @return display::Vector2i
+             */
             virtual display::Vector2i getPos() const = 0;
+            /**
+             * @brief Set the Size object
+             *
+             * @param size
+             */
             virtual void setSize(display::Vector2i size) = 0;
+            /**
+             * @brief Get the Size object
+             *
+             * @return display::Vector2i
+             */
             virtual display::Vector2i getSize() const = 0;
+            /**
+             * @brief Set the Texture object
+             *
+             * @param path
+             */
             virtual void setTexture(std::string path) = 0;
+            /**
+             * @brief Get the Texture object
+             *
+             * @return std::string
+             */
             virtual std::string getTexture() const = 0;
+            /**
+             * @brief Set the Character object
+             *
+             * @param c
+             */
             virtual void setCharacter(char c) = 0;
+            /**
+             * @brief Get the Character object
+             *
+             * @return char
+             */
             virtual char getCharacter() const = 0;
+            /**
+             * @brief Set the Character Color object
+             *
+             * @param color
+             */
             virtual void setCharacterColor(display::Color color) = 0;
+            /**
+             * @brief Get the Character Color object
+             *
+             * @return display::Color
+             */
             virtual display::Color getCharacterColor() const = 0;
+            /**
+             * @brief Set the Color object
+             *
+             * @param color
+             */
             virtual void setColor(display::Color color) = 0;
+            /**
+             * @brief Get the Color object
+             *
+             * @return display::Color
+             */
             virtual display::Color getColor() const = 0;
+            /**
+             * @brief Set the Text object
+             *
+             * @param text
+             */
             virtual void setText(char text) = 0;
+            /**
+             * @brief Get the Text object
+             *
+             * @return char
+             */
             virtual char getText() const = 0;
     };
 
+    /**
+     * @brief Rectangle class
+     * Main object used in our games, basically everything can be done using them
+     */
     class Rectangle : public IObject {
         public:
             Rectangle() {
@@ -61,15 +150,41 @@ namespace object {
             display::Color getCharacterColor() const override { return _characterColor;};
 
             //Other internal logic
+            /**
+             * @brief Get the Border Thickness object
+             *
+             * @return int
+             */
             int getBorderThickness() const {return _borderThickness;};
+            /**
+             * @brief Set the Border Thickness object
+             *
+             * @param thickness
+             */
             void setBorderThickness(int thickness) {_borderThickness = thickness;};
+            /**
+             * @brief Checks if a position is contained inside a the rectangle
+             * @param pos
+             * @return true
+             * @return false
+             */
             bool contains(display::Vector2i pos) const {
                 if (pos.x >= _pos.x && pos.x <= (_pos.x + _size.x) &&
                     pos.y >= _pos.y && pos.y <= (_pos.y + _size.y))
                     return true;
                 return false;
             }
+            /**
+             * @brief Get the Text object
+             *
+             * @return char
+             */
             char getText(void) const override {return _text;};
+            /**
+             * @brief Set the Text object
+             * The text object is used to prints character on the screen
+             * @param text
+             */
             void setText(char text) override {_text = text;};
         private:
             Type _id;
--- include/Loader.hpp
+++ include/Loader.hpp
@@ -32,17 +32,30 @@ namespace arcade {
                     dlclose(_gameLib);
             }
 
+            /**
+             * @brief Close the handler of the graphic library
+             */
             void closeGraphicLib() {
                 if (_graphicLib)
                     dlclose(_graphicLib);
                 _graphicLib = nullptr;
             }
+            /**
+             * @brief Close the handler of the game library
+             */
             void closeGameLib() {
                 if (_gameLib)
                     dlclose(_gameLib);
                 _gameLib = nullptr;
             };
 
+            /**
+             * @brief This function will open the lib based on the path passed
+             * in argument. MAKE SURE to check the return value of this function
+             * as this function will not throw in case of an error
+             * @param path
+             * @return void*
+             */
             void *loadLib(const std::string path) {
                 char *error = NULL;
                 void *handle = dlopen(path.c_str(), RTLD_LAZY);
@@ -50,15 +63,34 @@ namespace arcade {
                     return nullptr;
                 return handle;
             }
-
+            /**
+             * @brief Set the Display object
+             *
+             * @param display
+             */
             void setDisplay(void *display) {
                 _graphicLib = display;
             }
 
+            /**
+             * @brief Set the Game object
+             *
+             * @param game
+             */
             void setGame(void *game) {
                 _gameLib = game;
             }
-
+            /**
+             * @brief Get the Entry Point object
+             * This function return a certain type of library based on the handler
+             * and the name of the entryPoint passed in argument. MAKE SURE
+             * to check the return value of this function as it will not throw
+             * in case of an error
+             * @tparam T 
+             * @param handle 
+             * @param entryPoint 
+             * @return T 
+             */
             template<typename T>
             T getEntryPoint(void *handle, const char *entryPoint) const {
                 char *error = NULL;
